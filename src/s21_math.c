1f938ad7f89c65fd122169f375c794c2e9f47165
Tania Kiara <taniakia@un-m4.kzn.21-school.ru>
2023-01-12 16:17:17 +0300

    implemented abs and exp funcs

---
+++ src/s21_math.c
@@ -0,0 +1,13 @@
+int s21_abs(int x) { return x < 0 ? -x : x; }
+
+long double s21_exp(double x) {
+  long double x1 = 1.0f, precision = 0.000001, x, result = 0.0f;
+  int n = 0;
+
+  do {
+    result += x1;
+    x1 *= (x / ++n);
+  } while (x1 > precision);
+
+  return result;
+}
